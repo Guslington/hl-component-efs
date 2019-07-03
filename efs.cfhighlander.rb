3ab2bbd76bb0aec1a9e499fd9b4dd5e4e0d72fa7
Guslington <guslington@gmail.com>
2019-07-03 14:28:39 +1000

    no need for stackoctet parameter

--- efs.cfhighlander.rb
+++ efs.cfhighlander.rb
@@ -3,7 +3,6 @@ CfhighlanderTemplate do
   Parameters do
     ComponentParam 'EnvironmentName', 'dev', isGlobal: true
     ComponentParam 'EnvironmentType', 'development', isGlobal: true
-    ComponentParam 'StackOctet', '10', isGlobal: true
     ComponentParam 'VPCId', type: 'AWS::EC2::VPC::Id'
     ComponentParam "SubnetIds", type: 'CommaDelimitedList'
   end
